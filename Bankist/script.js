bf0b58cc76c06097dabee6aa4d079f7ef9fa1ae8
Scarlett <hao.sijia04@gmail.com>
2024-03-13 13:42:59 -0400

    sticky nav

--- Bankist/script.js
+++ Bankist/script.js
@@ -52,6 +52,30 @@ const handleHover = function (e) {
 nav.addEventListener("mouseover", handleHover.bind(0.5));
 nav.addEventListener("mouseout", handleHover.bind(1));
 
+//-------- Sticky navigation --------//
+// Intersection Observer API
+// const obsCallback = function (entries, observer) {
+//   entries.forEach();
+// };
+// const obsOptions = {
+//   root: null,
+//   threshold: [0,0.2],
+// };
+
+// const observer = new IntersectionObserver(obsCallback,obsOptions);
+// observer.observe(section1);
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  if(!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+}
+const headerObserver = new IntersectionObserver(stickyNav,{
+  root: null,
+  threshold:0,
+});
+headerObserver.observe(header);
+
+
 //-------- H1 Dom traversing --------//
 // h1.querySelectorAll('.highlight')
 h1.firstElementChild.style.color = "white";
